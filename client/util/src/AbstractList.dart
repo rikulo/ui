90f2bd16166e164546eaeb53959020050f19c9b0
Tom Yeh <tomyeh@zkoss.org>
2012-02-07 14:53:20 +0800

    Widget.children ready

---
+++ client/util/src/AbstractList.dart
@@ -0,0 +1,103 @@
+//Copyright (C) 2012 Potix Corporation. All Rights Reserved.
+//History: Tue Feb 7 10:42:56 TST 2012
+// Author: tomyeh
+
+/**
+ * A skeletal implementation of List.
+ * <p>For unmodifiable sequential list, you need only to implement
+ * [iterator] and [length].
+ * <p>If it is not a equential list, you shall override [operator[]]
+ * for better performance.
+ * <p>If you don't override [operator[]], you shall consider to override
+ * [last] for better performance.
+ * <p>For mutable list, you shall override [operator[]=], [set length],
+ * [add], [setRange], [insertRange], and [removeRange].
+ */
+class AbstractList<E> implements List<E> {
+
+	//Collection//
+	bool every(bool f(E element)) {
+		return Collections.every(this, f);
+	}
+	Collection map(f(E element)) {
+		return Collections.map(this, new GrowableObjectArray.withCapacity(length), f);
+	}
+	Collection filter(bool f(E element)) {
+		return Collections.filter(this, new GrowableObjectArray<E>(), f);
+	}
+	void forEach(f(E element)) {
+		Collections.forEach(this, f);
+	}
+	bool isEmpty() {
+		return this.length === 0;
+	}
+	bool some(bool f(E element)) {
+		return Collections.some(this, f);
+	}
+
+	//List//
+	E operator[](int index) {
+		Arrays.rangeCheck(this, index, 1);
+
+		final Iterator<E> it = iterator();
+		while (--index >= 0)
+			it.next();
+		return it.next();
+	}
+	void operator[]=(int index, E value) {
+		throw const UnsupportedOperationException("Cannot modify");
+	}
+	void set length(int newLength) {
+		throw const UnsupportedOperationException("Cannot modify");
+	}
+	void add(E element) {
+		throw const UnsupportedOperationException("Cannot modify");
+	}
+	void addLast(E element) {
+		add(element);
+	}
+	void addAll(Collection<E> elements) {
+		for (final E e in elements)
+			add(e);
+	}
+	void sort(int compare(E a, E b)) {
+		DualPivotQuicksort.sort(this, compare);
+	}
+	int indexOf(E element, [int start = 0]) {
+		return Arrays.indexOf(this, element, start, this.length);
+	}
+	int lastIndexOf(E element, [int start = null]) {
+		if (start === null) start = length - 1;
+		return Arrays.lastIndexOf(this, element, start);
+	}
+	void clear() {
+		removeRange(0, length);
+	}
+	E removeLast() {
+		removeRange(length - 1, 1);
+	}
+	E last() {
+		return this[length - 1];
+	}
+	List<E> getRange(int start, int length) {
+		if (length == 0) return [];
+		Arrays.rangeCheck(this, start, length);
+		List list = new List<E>();
+		list.length = length;
+		Arrays.copy(this, start, list, 0, length);
+		return list;
+	}
+	void setRange(int start, int length, List<E> from, [int startFrom = 0]) {
+		throw const UnsupportedOperationException("Cannot modify");
+	}
+	void removeRange(int start, int length) {
+		throw const UnsupportedOperationException("Cannot modify");
+	}
+	void insertRange(int start, int length, [E initialValue = null]) {
+		throw const UnsupportedOperationException("Cannot modify");
+	}
+
+	String toString() {
+		return Arrays.asString(this);
+	}
+}
---
+++ client/widgetimpl/src/WidgetChildren.dart
@@ -0,0 +1,143 @@
+//Copyright (C) 2012 Potix Corporation. All Rights Reserved.
+//History: Tue Feb  7 09:08:49 TST 2012
+// Author: tomyeh
+
+/**
+ * A list of child widgets.
+ * Notice that [set length] are not supported
+ */
+class WidgetChildren extends AbstractList<Widget> {
+	final Widget _owner;
+
+	WidgetChildren(this._owner);
+
+	//Iterable//
+	Iterator<Widget> iterator() {
+		return new _WCIterator(_owner);
+	}
+
+	//Collection//
+	int get length() => _owner.childCount;
+
+	//List//
+	void operator[]=(int index, Widget value) {
+		if (value === null)
+			throw const IllegalArgumentException("null");
+		final Widget w = this[index];
+		if (w !== value) {
+			final Widget next = w.nextSibling;
+			_owner.removeChild(w);
+			_owner.insertBefore(value, next);
+		}
+	}
+	void add(Widget widget) {
+		_owner.appendChild(widget);
+	}
+	void sort(int compare(Widget a, Widget b)) {
+		List<Widget> copy = new List.from(this);
+		copy.sort(compare);
+print("${copy}");
+		setRange(0, length, copy);
+	}
+	Widget removeLast() {
+		final Widget last = last();
+		if (last != null)
+			_owner.removeChild(last);
+		return last;
+	}
+	Widget last() {
+		return _owner.lastChild;
+	}
+	void setRange(int start, int length, List<Widget> from, [int startFrom = 0]) {
+		if (length <= 0)
+			return; //nothing to do
+
+		if (start > this.length) {
+			throw new IndexOutOfRangeException(start);
+		} else if (start == this.length) { //append
+			if (startFrom == 0) { //optimize
+				for (Iterator<Widget> it = from.iterator(); --length >= 0;)
+					add(it.next());
+			} else {
+				while (--length >= 0)
+					add(from[startFrom++]);
+			}
+		} else if (startFrom == 0) { //optimize
+			Widget w = this[start];
+			Iterator<Widget> it = from.iterator();
+			while (--length >= 0) { //replace
+				Widget value = it.next();
+				final Widget next = w.nextSibling;
+				if (w !== value) {
+					_owner.removeChild(w);
+					_owner.insertBefore(value, next);
+				}
+				if (next === null)
+					break;
+				w = next;
+			}
+			while (--length >= 0) //append
+				add(it.next());
+		} else {
+			Widget w = this[start];
+			while (--length >= 0) { //replace
+				Widget value = from[startFrom++];
+				final Widget next = w.nextSibling;
+				if (w !== value) {
+					_owner.removeChild(w);
+					_owner.insertBefore(value, next);
+				}
+				if (next === null)
+					break;
+				w = next;
+			}
+			while (--length >= 0) //append
+				add(from[startFrom++]);
+		}
+	}
+	void removeRange(int start, int length) {
+		if (length <= 0)
+			return; //nothing to do
+		Arrays.rangeCheck(this, start, 1); //length OK to be greater
+
+		Widget child = _owner.firstChild;
+		while (--index >= 0)
+			child = child.nextSibling;
+		while (--length >= 0 && child != null) {
+			Widget next = child.nextSibling;
+			_owner.removeChild(child);
+			child = next;
+		}
+	}
+	void insertRange(int start, int length, [Widget initialValue = null]) {
+		if (length != 1)
+			throw const IllegalArgumentException("Allow only one widget");
+		if (initialValue === null)
+			throw const IllegalArgumentException("Require initialValue");
+		if (start == this.length) {
+			add(initialValue);
+		} else {
+			Widget w = this[start];
+			_owner.insertBefore(initialValue, w);
+		}
+	}
+}
+
+class _WCIterator implements Iterator<Widget> {
+	Widget _next;
+
+	_WCIterator(Widget owner) {
+		_next = owner.firstChild;
+	}
+
+	bool hasNext() {
+		return _next != null;
+	}
+	Widget next() {
+		if (_next === null)
+			throw const NoMoreElementsException();
+		Widget next = _next;
+		_next = _next.nextSibling;
+		return next;
+	}
+}
