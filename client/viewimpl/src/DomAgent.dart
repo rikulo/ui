dd4d4ff8958ccc308ed612d263f5c2d6b10f7fd2
tomyeh <tomyeh@zkoss.org>
2012-04-21 22:44:46 +0800

    finetune

--- client/viewimpl/src/DomAgent.dart
+++ client/viewimpl/src/DomAgent.dart
@@ -19,12 +19,9 @@ class DomAgent {
 		do {
 			ofs.left += el.$dom_offsetLeft;
 			ofs.top += el.$dom_offsetTop;
-			if (el.style.position == "fixed") {
-				//No need to add widnow's innerLeft/Top since we don't allow
-				//window-level scrolling (rather, rootView is scrolled)
-				break;
-			}
-		} while ((el = el.offsetParent) != null);
+		} while (el.style.position != "fixed" && (el = el.offsetParent) != null);
+		//Note: no need to add widnow's innerLeft/Top if position is fixed.
+		//reason: we don't allow window-level scrolling (rather, rootView is scrolled)
 		return ofs;
 	}
 	/** Returns the final used values of all the CSS properties
