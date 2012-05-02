5a2d302325b9dddc4d7495808c299b71525a5a00
tomyeh <tomyeh@zkoss.org>
2012-05-02 13:10:29 +0800

    Decalration.text ready

--- samples/helloworld/HelloWorld.dart
+++ samples/helloworld/HelloWorld.dart
@@ -10,8 +10,7 @@ class HelloWorld extends Activity {
 		title = "Hello World!";
 
 		TextView text = new TextView("Hello World!");
-		text.profile.anchorView = rootView;
-		text.profile.location = "center center";
+		text.profile.text = "anchor:  parent; location: center center";
 		text.on.click.add((event) {
 			event.target.style.border =
 				event.target.style.border.isEmpty() ? "1px solid blue": "";
