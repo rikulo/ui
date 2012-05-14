dba015b08be1ba293962973cebe219f8faf50955
Henri Chen <henrichen@zkoss.org>
2012-05-14 14:19:25 +0800

    fine tune

---
+++ client/deviceimpl/deviceimpl.dart
@@ -0,0 +1,8 @@
+//Copyright (C) 2012 Potix Corporation. All Rights Reserved.
+//History: Tue, Apr 17, 2012 12:49:39 PM
+// Author: tomyeh
+
+#library("rikulo:deviceimpl");
+
+#source("src/DeviceEvent.dart");
+#source("src/DeviceEvents.dart");
