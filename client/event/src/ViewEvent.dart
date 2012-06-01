80fc012606f49f4774b8433b156117a9e863b0ec
tomyeh <tomyeh@zkoss.org>
2012-06-01 22:38:36 +0800

    ViewEvent.offset is initialized on-demand

--- client/event/src/ViewEvent.dart
+++ client/event/src/ViewEvent.dart
@@ -16,47 +16,72 @@ typedef void ViewEventListener(ViewEvent event);
  */
 class ViewEvent {
 	View _target;
-	final UIEvent _domEvt;
+	final Event _domEvt;
 	final String _type;
 	final int _stamp;
-	/** The offset relative to [target]'s coordinate.
-	 */
-	Offset offset;
+	Offset _offset;
+	bool _offsetReady = false;
 	bool _propStop = false;
 
 	/** Constructor.
 	 *
 	 * <p>[target] is the view that this event is targeting.
 	 * [type] is the event type, such as click.
-	 * <p>[pageX] and [pageY] is the mouse pointer relative to the document.
-	 * If this event is constructed from a DOM event, it is
+	 * <p>[pageX] and [pageY] are the mouse pointer relative to the document.
+	 * They are ignored if not specified.
+	 * <p>[offsetX] and [offsetY] are the mouse pointer relative to [target]'s
+	 * left-top corner.
+	 * They are ignored if [pageX] and [pageY] are specified.
+	 * If both [offsetX] and [pageX] are not specified, 0 is assumed.
+	 * If this event is constructed from a DOM event (UIEvent),
+	 * it is UIEvent.pageX and UIEvent.pageY.
 	 */
-	ViewEvent(View target, String type, [int pageX=0, int pageY=0]):
+	ViewEvent(View target, String type, [int pageX, int pageY, int offsetX, int offsetY]):
 	_domEvt = null, _type = type, _stamp = new Date.now().value {
 		if (type == null)
 			throw const UIException("type required");
 		_target = currentTarget = target;
-		_updateOffset(pageX, pageY);
+
+		if (pageX !== null && pageY !== null) {
+			_offset = new Offset(pageX, pageY);
+		} else {
+			_offset = new Offset(offsetX !== null ? offsetX: 0, offsetY !== null ? offsetY: 0);
+			_offsetReady = true;
+		}
 	}
 	/** Constructs a view event from a DOM event.
 	 * It is rarely called unless you'd like to wrap a DOM event.
 	 */
-	ViewEvent.dom(View target, UIEvent domEvent, [String type]) : 
+	ViewEvent.dom(View target, Event domEvent, [String type]) : 
 	_domEvt = domEvent, _type = type != null ? type: domEvent.type,
 	_stamp = domEvent.timeStamp {
 		_target = currentTarget = target;
-		_updateOffset(domEvent.pageX, domEvent.pageY);
+		_offset = new Offset(0, 0);
 	}
-	void _updateOffset(int pageX, int pageY) {
-		offset = new Offset(pageX, pageY);
-		try {
-			if (pageX != 0 || pageY != 0) {
+
+	/** The offset relative to [target]'s coordinate.
+	 */
+	Offset get offset() {
+		if (!_offsetReady) {
+			_offsetReady = true;
+			try {
+				if (domEvent !== null) {
+					if (domEvent is! UIEvent)
+						return _offset;
+
+					final UIEvent uievt = domEvent;
+					_offset.x = uievt.pageX;
+					_offset.y = uievt.pageY;
+				}
+
 				final Offset ofs = new DOMQuery(target).documentOffset;
-				offset.left -= ofs.left;
-				offset.top -= ofs.top;
+				_offset.left -= ofs.left;
+				_offset.top -= ofs.top;
+			} catch (final e) {
+				print("Faile to get offset for $this, $e");
 			}
-		} catch (final e) {
 		}
+		return _offset;
 	}
 
 	/** Returns the view that this event is targeting  to.
@@ -68,7 +93,7 @@ class ViewEvent {
 
 	/** The DOM event that causes this view event, or null if not available.
 	 */
-	UIEvent get domEvent() => _domEvt;
+	Event get domEvent() => _domEvt;
 
 	/** Returns the time stamp. */
 	int get timeStamp() => _stamp;
