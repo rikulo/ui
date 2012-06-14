7f5ddbf577ba5d8ad9ffc25dcc4fa8f937efbb75
tomyeh <tomyeh@zkoss.org>
2012-06-14 14:17:41 +0800

    revert

---
+++ client/view/src/UIException.dart
@@ -0,0 +1,11 @@
+//Copyright (C) 2012 Potix Corporation. All Rights Reserved.
+//Jan. 10, 2012
+
+/** An UI exception.
+ */
+class UIException implements Exception {
+  final String message;
+
+  const UIException(String this.message);
+  String toString() => "UIException($message)";
+}
