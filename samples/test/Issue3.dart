4ac78a1c34e73476a1690db1f6a7a93c952170a9
tomyeh <tomyeh@zkoss.org>
2012-06-28 14:05:03 +0800

    Issue 3: View.hidden not work properly

---
+++ samples/test/Issue3.dart
@@ -0,0 +1,41 @@
+#import('../../client/app/app.dart');
+#import('../../client/view/view.dart');
+
+class Issue3 extends Activity {
+  View _left, _right;
+  void onCreate_() {
+    mainView.layout.type = "linear";
+    mainView.layout.orient = "vertical";
+
+    //hlayout
+    View hlayout = new View();
+    hlayout.layout.type = "linear";
+    hlayout.layout.orient = "horizontal";
+    hlayout.profile.width = "100%";
+    hlayout.profile.height = "content";
+    mainView.addChild(hlayout);
+
+    //left text
+    TextView left = new TextView("FIRST TEXT");
+    hlayout.addChild(left);
+    _left = left;
+
+    //right text
+    TextView right = new TextView("SECOND TEXT");
+    right.hidden = true;
+    hlayout.addChild(right);
+    _right = right;
+
+    _left.on.click.add((event) {_left.hidden = true; _right.hidden = false; mainView.requestLayout();});
+    _right.on.click.add((event) {_left.hidden = false; _right.hidden = true; mainView.requestLayout();});
+
+    mainView.addChild(new TextView.html('''
+        <ul><li>Click FIRST TEXT and you shall see it disappears and SECOND TEXT shows up</li>
+        <li>Click SECOND TEXT and you shall see the reversed response</li></ul>
+        '''));
+  }
+}
+
+main() {
+  new Issue3().run();
+}
