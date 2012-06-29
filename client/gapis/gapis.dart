531d199d67a388f8ac7715519bebbda29a769d66
Henri Chen <henrichen@zkoss.org>
2012-06-30 01:21:19 +0800

    Add Google APIs bridge

---
+++ client/gapis/gapis.dart
@@ -0,0 +1,12 @@
+//Copyright (C) 2012 Potix Corporation. All Rights Reserved.
+//History: Wed, Jun 20, 2012  10:28:07 AM
+// Author: hernichen
+
+#library("rikulo:gapis");
+
+#import("dart:html");
+#import("../util/util.dart");
+
+#source("src/LoadableModule.dart");
+#source("src/GLoader.dart");
+#source("src/GFeed.dart");
