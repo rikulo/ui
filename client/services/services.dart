b1da9cfaa0be1f35e88f79a9385e546025f5bf5b
Henri Chen <henrichen@zkoss.org>
2012-07-03 16:29:53 +0800

    SmartIP service (IP -> Geo information)

---
+++ client/services/services.dart
@@ -0,0 +1,12 @@
+//Copyright (C) 2012 Potix Corporation. All Rights Reserved.
+//History: Wed, Jun 20, 2012  05:21:28 PM
+// Author: hernichen
+
+#library("rikulo:services");
+
+#import("dart:html");
+#import("dart:json");
+#import("../html/html.dart");
+#import("../util/util.dart");
+
+#source("src/SmartIP.dart");
---
+++ samples/test/TestSmartIP.dart
@@ -0,0 +1,29 @@
+#import('../../client/app/app.dart');
+#import('../../client/view/view.dart');
+#import('../../client/services/services.dart');
+#import('../../client/util/util.dart');
+
+class TestSmartIP extends Activity {
+
+  void onCreate_() {
+    SmartIP loader = new SmartIP();
+    loader.loadIPGeoInfo((Map geoip) {
+        log('''
+          source: ${geoip['source']},
+          host: ${geoip['host']},
+          lang: ${geoip['lang']},
+          countryName: ${geoip['countryName']},
+          countryCode: ${geoip['countryCode']},
+          city: ${geoip['city']},
+          region: ${geoip['region']},
+          latitude: ${geoip['latitude']}, 
+          longitude: ${geoip['longitude']},
+          timezone: ${geoip['timezone']}
+        ''');
+    });
+  }
+}
+
+void main() {
+  new TestSmartIP().run();
+}
