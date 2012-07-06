b9059570ef71a1f2e37fc045d2f3ba4cdbd72fd8
tomyeh <tomyeh@zkoss.org>
2012-07-06 14:13:29 +0800

    dartdoc

--- samples/dartdoc/dartdoc.dart
+++ samples/dartdoc/dartdoc.dart
@@ -3,6 +3,14 @@
 
 #import("../../client/app/app.dart");
 #import("../../client/device/device.dart");
+#import("../../client/device/accelerometer/accelerometer.dart");
+#import("../../client/device/camera/camera.dart");
+#import("../../client/device/capture/capture.dart");
+#import("../../client/device/connection/connection.dart");
+#import("../../client/device/compass/compass.dart");
+#import("../../client/device/contacts/contacts.dart");
+#import("../../client/device/geolocation/geolocation.dart");
+#import("../../client/device/notification/notification.dart");
 #import("../../client/effect/effect.dart");
 #import("../../client/event/event.dart");
 #import("../../client/html/html.dart");
