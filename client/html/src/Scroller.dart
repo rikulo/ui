2285f83b7bb53377a8b686ec6cf64db0ef33a988
simonpai <simonpai@potix.com>
2012-07-09 17:22:26 +0800

    Add scroll bar feature to Scroller.

--- client/html/src/Scroller.dart
+++ client/html/src/Scroller.dart
@@ -23,19 +23,32 @@ interface Scroller default _Scroller {
    * + [dir]: the direction. If not specified, [Dir.BOTH] is assumed.
    */
   Scroller(Element owner, AsSize viewPortSize, [Element handle, Dir direction, 
-      AsSize contentSize, ScrollerStart start, ScrollerMove moving, ScrollerMove end]);
-  // TODO: inertial, bounce, scrollbar
+      AsSize contentSize, bool scrollbar, 
+      ScrollerStart start, ScrollerMove moving, ScrollerMove end]);
+  // TODO: inertial, bounce
   
   /** Destroys the scroller.
    * It shall be called to clean up the scroller, if it is no longer used.
    */
   void destroy();
   
+  /** Returns the size of view port.
+   */
+  Size get viewPortSize();
+  
+  /** Returns the content size.
+   */
+  Size get contentSize();
+  
   /** Returns the direction that the scrolling is allowed.
    */
   Dir get direction();
 }
 
+/**
+ * The state in a scolling process, provided by [Scroller] in [ScrollerStart] and
+ * [ScrollerMove] callback.
+ */
 interface ScrollerState default _ScrollerState {
   
   /** Returns the associated [Scroller].
@@ -53,6 +66,33 @@ interface ScrollerState default _ScrollerState {
   /** Returns the latest timestamp at which the scroll position is updated.
    */
   int get time();
+  
+}
+
+/**
+ * The controller of a virtual scroll bar which appears when scrolling.
+ */
+interface ScrollbarControl default _ScrollbarControl {
+  
+  /** Initialization of scroll bar.
+   */
+  void init(bool vertical);
+  
+  /** Destory the scroll bar.
+   */
+  void destroy(bool vertical);
+  
+  /** Called when scrolling starts.
+   */
+  void start(bool vertical, num position, num velocity, int time);
+  
+  /** Called at each scrolling iteraion.
+   */
+  void move(bool vertical, num position, num velocity, int time);
+  
+  /** Called when scrolling stops.
+   */
+  void end(bool vertical, num position, num velocity, int time);
   
 }
 
@@ -75,14 +115,90 @@ class _ScrollerState implements ScrollerState {
     if (_time == null || time > _time) {
       _ppos = _pos;
       _ptime = _time;
-      _pos = pos;
+      _pos = new Offset(-pos.x, -pos.y);
       _time = time;
     }
   }
   
-  Offset get velocity() => _ppos == null ? new Offset(0, 0) : (_pos - _ppos) / (_time - _ptime);
+  Offset get velocity() => _ppos == null || _pos == null || _time == null || _ptime == null ? 
+      new Offset(0, 0) : ((_pos - _ppos) / (_time - _ptime));
   
   int get time() => _time;
+  
+}
+
+class _ScrollbarControl implements ScrollbarControl {
+  
+  final Scroller scroller;
+  final Element owner;
+  Element _hbar, _vbar;
+  
+  _ScrollbarControl(this.scroller, this.owner);
+  
+  /* bar inner size: 4px
+   * bar border: 1px
+   * bar margin: 2px
+   */
+  static final _mgs = 2, _bds = 1, _ins = 4;
+  
+  void init(bool vertical) {
+    if (vertical) {
+      _vbar = new Element.tag("div");
+      _vbar.classes = ["v-scrollbar-ver"];
+      _vbar.style.width = CSS.px(_ins); // do here to have better sync
+      owner.parent.insertBefore(_vbar, owner.nextElementSibling);
+    } else {
+      _hbar = new Element.tag("div");
+      _hbar.classes = ["v-scrollbar-hor"];
+      _hbar.style.height = CSS.px(_ins); // do here to have better sync
+      owner.parent.insertBefore(_hbar, owner.nextElementSibling);
+    }
+  }
+  
+  void destroy(bool vertical) {
+    if (vertical) {
+      _vbar.remove();
+      _vbar == null;
+    } else {
+      _hbar.remove();
+      _hbar == null;
+    }
+  }
+  
+  void start(bool vertical, num position, num velocity, int time) {
+    _updateBar(vertical, position);
+    final Element bar = vertical ? _vbar : _hbar;
+    bar.style.display = "block"; // TODO: animation + leave hook to cancel
+  }
+  
+  void move(bool vertical, num position, num velocity, int time) {
+    _updateBar(vertical, position);
+  }
+  
+  void end(bool vertical, num position, num velocity, int time) {
+    final Element bar = vertical ? _vbar : _hbar;
+    bar.style.display = "none"; // TODO: animation + leave hook to skip
+  }
+  
+  void _updateBar(bool ver, num pos) {
+    final Size csize = scroller.contentSize;
+    final Size vsize = scroller.viewPortSize;
+    final num csize0 = ver ? csize.height : csize.width;
+    final num vsize0 = ver ? vsize.height : vsize.width;
+    final num s = ((vsize0 - _mgs * 2) * (csize0 > vsize0 ? vsize0 / csize0 : 1)).toInt() - _bds * 2;
+    final num x = _mgs + (csize0 > vsize0 ? ((vsize0 - _mgs * 2) * pos / csize0) : 0);
+    final num off = (ver ? vsize.width : vsize.height) - _mgs - _ins - _bds * 2;
+    
+    if (ver) {
+      _vbar.style.height = CSS.px(s);
+      _vbar.style.top = CSS.px(x);
+      _vbar.style.left = CSS.px(off);
+    } else {
+      _hbar.style.width = CSS.px(s);
+      _hbar.style.left = CSS.px(x);
+      _hbar.style.top = CSS.px(off);
+    }
+  }
   
 }
 
@@ -92,6 +208,7 @@ class _ScrollerState implements ScrollerState {
 class _Scroller implements Scroller {
   final Element owner, handle;
   final Dir direction;
+  final bool scrollbar;
   final ScrollerStart _start;
   final ScrollerMove _end, _moving;
   final AsSize _fnContentSize, _fnViewPortSize;
@@ -99,11 +216,14 @@ class _Scroller implements Scroller {
   DragGesture _dg;
   _BoundedInertialMotion _bim;
   _ScrollerState _state;
+  ScrollbarControl _scrollbarCtrl;
   
-  _Scroller(this.owner, this._fnViewPortSize, [Element handle, Dir direction = Dir.BOTH, 
-      AsSize contentSize, ScrollerStart start, ScrollerMove moving, ScrollerMove end]) :
-      this.handle = handle, this.direction = direction, _fnContentSize = contentSize, 
-      _start = start, _moving = moving, _end = end {
+  _Scroller(this.owner, this._fnViewPortSize, [Element handle, 
+  Dir direction = Dir.BOTH, AsSize contentSize, bool scrollbar = true, 
+  ScrollerStart start, ScrollerMove moving, ScrollerMove end]) :
+  this.handle = handle, this.direction = direction, this.scrollbar = scrollbar,
+  _fnContentSize = contentSize, 
+  _start = start, _moving = moving, _end = end {
     
     // TODO: transform
     _dg = new DragGesture(this.owner, handle: handle,
@@ -115,6 +235,13 @@ class _Scroller implements Scroller {
       _bim = new _BoundedInertialMotion(owner, state.velocity, range, moving: onMoving, end: onEnd);
     });
     
+    // init scroll bar
+    if (scrollbar) {
+      _scrollbarCtrl = _scrollbarControl();
+      if (_scrollbarCtrl != null)
+        _applyScrollBarFunction0(_scrollbarCtrl.init);
+    }
+    
     //TODO: support desktop - if not in simulator, mousewheel/draggable scrollbar
   }
   
@@ -122,11 +249,15 @@ class _Scroller implements Scroller {
     if (_bim != null)
       _bim.stop();
     _state = new _ScrollerState(this, time);
+    if (scrollbar && _scrollbarCtrl != null)
+      _applyScrollBarFunction1(_scrollbarCtrl.start, _state);
     return _start == null || _start(_state);
   }
   
   void onMoving(Offset position, int time) {
     _state.snapshot(position, time);
+    if (scrollbar && _scrollbarCtrl != null)
+      _applyScrollBarFunction1(_scrollbarCtrl.move, _state);
     if (_moving != null)
       _moving(_state);
   }
@@ -134,7 +265,26 @@ class _Scroller implements Scroller {
   void onEnd() {
     if (_end != null)
       _end(_state);
+    if (scrollbar && _scrollbarCtrl != null)
+      _applyScrollBarFunction1(_scrollbarCtrl.end, _state);
     _state = null;
+  }
+  
+  // scroll bar //
+  ScrollbarControl _scrollbarControl() => new _ScrollbarControl(this, this.owner);
+  
+  void _applyScrollBarFunction0(Function f) {
+    if (this.direction == Dir.HORIZONTAL || this.direction == Dir.BOTH)
+      f(false);
+    if (this.direction == Dir.VERTICAL || this.direction == Dir.BOTH)
+      f(true);
+  }
+  
+  void _applyScrollBarFunction1(Function f, ScrollerState state) {
+    if (this.direction == Dir.HORIZONTAL || this.direction == Dir.BOTH)
+      f(false, state.position.x, state.velocity.x, state.time);
+    if (this.direction == Dir.VERTICAL || this.direction == Dir.BOTH)
+      f(true, state.position.y, state.velocity.y, state.time);
   }
   
   // size cache //
@@ -170,6 +320,8 @@ class _Scroller implements Scroller {
   
   void destroy() {
     _state = null;
+    if (scrollbar && _scrollbarCtrl != null)
+      _applyScrollBarFunction0(_scrollbarCtrl.destroy);
     _dg.destroy();
   }
   
