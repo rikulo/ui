6e5f1ed383546e665ec76c6ec0964c2583b2fd77
simonpai <simonpai@potix.com>
2012-07-10 16:54:38 +0800

    Add ScrollView test case.

---
+++ samples/scroll-view/ScrollViewDemo2.dart
@@ -0,0 +1,39 @@
+//Sample Code: ScrollView
+
+#import('../../client/app/app.dart');
+#import('../../client/util/util.dart');
+#import('../../client/view/view.dart');
+#import('../../client/html/html.dart');
+
+class ScrollViewDemo2 extends Activity {
+  
+  void onCreate_() {
+    title = "ScrollView Demo";
+
+    final ScrollView view = new ScrollView(direction: Dir.VERTICAL);
+    view.profile.text =
+      "anchor: parent; location: center center; width: 80%; height: 80%";
+    view.classes.add("scroll-view");
+
+    for (int x = 0; x < 50; ++x) {
+      View child = new View();
+      TextView txtv = new TextView("Row $x");
+      
+      final String color = CSS.color(250 - x * 4, 250, 200);
+      child.style.cssText = "border: 1px solid #AAAAAA; background-color: #FFFFFF; color: #111111; width: 100%; line-height: 50px";
+      child.style.userSelect = "none";
+      child.top = x * 50;
+      child.height = 50;
+      
+      child.addChild(txtv);
+      view.addChild(child);
+    }
+    
+    mainView.addChild(view);
+  }
+  
+}
+
+void main() {
+  new ScrollViewDemo2().run();
+}
