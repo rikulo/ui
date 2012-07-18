3dc30412a9e9d05935bce7816315a57706b1996c
tomyeh <tomyeh@zkoss.org>
2012-07-18 16:20:39 +0800

    test case

---
+++ samples/test/TestPartial.dart
@@ -0,0 +1,23 @@
+//Sample Code: activity is just a part of whole HTML page
+
+#import('../../client/app/app.dart');
+#import('../../client/view/view.dart');
+
+class TestPartial extends Activity {
+
+  void onCreate_() {
+    TextView welcome = new TextView("Hello World!");
+    welcome.profile.text = "anchor:  parent; location: center center";
+    welcome.on.click.add((event) {
+      TextView v = new TextView.html('<ul style="padding:10px"><li>Dialog Popup</li></ul>');
+      v.classes.add("v-dialog");
+      v.profile.location = "center center";
+      addDialog(v);
+    });
+    mainView.addChild(welcome);
+  }
+}
+
+void main() {
+  new TestPartial().run();
+}
