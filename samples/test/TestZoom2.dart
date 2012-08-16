b6f8970300bc1899ab72fddf7de5300516e58bdf
simonpai <simonpai@potix.com>
2012-08-16 15:27:28 +0800

    Internalize img resource in demo.

--- samples/test/TestZoom2.dart
+++ samples/test/TestZoom2.dart
@@ -24,7 +24,7 @@ class TestZoom2 extends Activity {
     Image img = new Image();
     // http://www.flickr.com/photos/normanbleventhalmapcenter/2675391188/
     // Creative Commons License
-    img.src = "http://farm4.staticflickr.com/3140/2675391188_d42c03b423.jpg";
+    img.src = "res/dutch-west-india-company-map.jpg";
     img.profile.text = "location: center center";
     
     panel.addChild(img);
