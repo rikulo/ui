b15b9203caf4394561ddab2015f5f0db1ea543f7
simonpai <simonpai@potix.com>
2012-08-20 16:43:06 +0800

    Add VelocityProvider.

--- client/util/src/Offset.dart
+++ client/util/src/Offset.dart
@@ -113,3 +113,38 @@ class _Offset3d extends _Offset implements Offset3d {
   int hashCode() => (x + y + z).toInt();
   String toString() => "($x, $y, $z)";
 }
+
+/** An utility that supplies velocity based on given snapshot of position-time
+ * pairs. This utility is designed to avoid calculating velocity from an 
+ * excessively small denominator. 
+ */
+class VelocityProvider {
+  
+  final int _threshold;
+  Offset _pos, _vel = new Offset(0, 0);
+  int _time;
+  
+  /** Initialize the provider with current time and position, which guarantees
+   * to supply velocity value from calculation that uses a denominator greater 
+   * than the [threshold]. As a tradeoff, the velocity will only be updated
+   * roughly in a period of [threshold] milliseconds.
+   */
+  VelocityProvider(Offset position, int time, [int threshold = 0]) : 
+    _pos = position, _time = time, _threshold = max(threshold, 0);
+  
+  /** Provide latest position and time.
+   */
+  void snapshot(Offset position, int time) {
+    final int diffTime = time - _time;
+    if (diffTime > _threshold) {
+      _vel = (position - _pos) / diffTime;
+      _time = time;
+      _pos = position;
+    }
+  }
+  
+  /** Retrieve velocity.
+   */
+  Offset get velocity() => _vel;
+  
+}
