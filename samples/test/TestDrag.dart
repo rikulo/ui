cda7383aae48868567d6d7b232a19698fa210959
simonpai <simonpai@potix.com>
2012-08-21 11:26:57 +0800

    Gesture refactoring: update test case to use Dragger.

--- samples/test/TestDrag.dart
+++ samples/test/TestDrag.dart
@@ -7,30 +7,36 @@
 #import('../../client/util/util.dart');
 
 class TestDrag extends Activity {
-
+  
+  Rectangle range;
+  
   void onCreate_() {
     _createBoxedDrag(mainView);
     _createSimpleDrag(mainView);
- }
+  }
   View _createSimpleDrag(View parent) {
     View view = _createDragView(parent, "Simple Drag");
-    new DragGesture(view.node);
+    new Dragger(view.node);
     return view;
   }
   View _createBoxedDrag(View parent) {
     View box = new View();
     box.classes.add("drag");
-    box.profile.text =
-      "location: center center; width: 70%; height: 70%";
+    box.profile.text = "location: center center; width: 70%; height: 70%";
     parent.addChild(box);
-
     View view = _createDragView(box, "Boxed Drag");
-    final DOMQuery qbox = new DOMQuery(box),
-      qview = new DOMQuery(view);
-    new DragGesture(view.node,
-      range: () => new Rectangle(0, 0,
-        qbox.innerWidth - qview.outerWidth,
-        qbox.innerHeight - qview.outerHeight));
+    
+    new Dragger(view.node, snap: (Offset ppos, Offset pos) => range.snap(pos));
+    
+    view.on.layout.add((event) {
+      final Size vs = new DOMQuery(view).outerSize;
+      final Size bs = new DOMQuery(box).innerSize;
+      range = new Rectangle(0, 0, bs.width - vs.width, bs.height - vs.height);
+      final Offset vpos = range.snap(new DOMQuery(view).offset);
+      view.left = vpos.left;
+      view.top = vpos.top;
+    });
+    
     return box;
   }
   TextView _createDragView(View parent, String label) {
