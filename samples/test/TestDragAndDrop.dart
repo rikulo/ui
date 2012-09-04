73903bc4e9a73feaf463019b1e4c47675fea1546
tomyeh <tomyeh@zkoss.org>
2012-09-04 14:06:36 +0800

    finetune TestDragAndDrop to reject the data dropped from other sources, such as other browsers/applications

--- samples/test/TestDragAndDrop.dart
+++ samples/test/TestDragAndDrop.dart
@@ -30,48 +30,52 @@ class TestDragAndDrop extends Activity {
     //create drag effect
     ..dragStart.add((event) {
       event.target.classes.add("dragged");
-      event.dataTransfer.setData("Text", event.target.uuid);
-        //IE accepts only "Text" and "URL" as the first argument (since it is format)
+      event.dataTransfer.setData("Text", "uuid:${event.target.uuid}");
+        //IE accepts only "Text" and "URL" as the first argument
     })
     ..dragEnd.add((event) {
       event.target.classes.remove("dragged");
     })
     //allow drop if it is in a container
     ..dragOver.add((event) {
-      if (getContainer(event.target) != null)
+      if (getContainer(event) != null)
         event.preventDefault();
         //calling preventDefault in dragOver means "allow drop"
     })
     //create allow-drop effect
     ..dragEnter.add((event) {
-      final container = getContainer(event.target);
+      final container = getContainer(event);
       if (container != null)
         window.setTimeout((){container.classes.add("dragover");}, 0);
         //Chrome issue: prev.dragLeave is fired after nextdragEnter, so
         //we have to defer dragEnter to make the sequence: leave and then enter
     })
     ..dragLeave.add((event) {
-      final container = getContainer(event.target);
+      final container = getContainer(event);
       if (container != null)
         container.classes.remove("dragover");
     })
     //handle drop
     ..drop.add((event) {
-      final container = getContainer(event.target);
+      final container = getContainer(event);
       if (container != null) {
         container.classes.remove("dragover");
           //Chrome issue: dragLeave not called, so clean up here
-        container.addChild(ViewUtil.getView(event.dataTransfer.getData("Text")));
+        final data = event.dataTransfer.getData("Text");
+        container.addChild(ViewUtil.getView(data.substring(5).trim())); //trim uuid:
         container.requestLayout();
       }
     });
 
     mainView.addChild(new TextView("Drag views between two containers"));
   }
-  static View getContainer(View view) {
-    for (; view != null; view = view.parent)
-      if (view.classes.contains('container'))
-        return view;
+  static View getContainer(Event event) {
+    //to protect the drop from other source, we have to check data first
+    final data = event.dataTransfer.getData("Text");
+    if (data != null && data.startsWith("uuid:"))
+      for (View view = event.target; view != null; view = view.parent)
+        if (view.classes.contains('container'))
+          return view;
   }
 }
 
