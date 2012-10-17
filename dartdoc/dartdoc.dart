18854f648cfeecef9bc0f6c969f9a5d4bedea714
Tom Yeh <tomyeh@zkoss.org>
2012-10-17 17:01:32 +0800

    enhance d2d

---
+++ dartdoc/dartdoc.dart
@@ -0,0 +1,20 @@
+//List all packages that shall be part of Rikulo API Reference
+library _____;
+
+import '../lib/effect.dart';
+import '../lib/event.dart';
+import '../lib/gesture.dart';
+import '../lib/html.dart';
+import '../lib/layout.dart';
+import '../lib/message.dart';
+import '../lib/model.dart';
+import '../lib/service.dart';
+import '../lib/service/gapis.dart';
+import '../lib/service/gmaps.dart';
+import '../lib/service/yapis.dart';
+import '../lib/util.dart';
+import '../lib/view.dart';
+import '../lib/view/impl.dart';
+import '../lib/view/select.dart';
+
+void main() {}
