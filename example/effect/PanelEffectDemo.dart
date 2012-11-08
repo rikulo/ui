c00a9a8d59f83dc9c844dab7f2da66a81685faab
simonpai <simonpai@potix.com>
2012-11-08 12:45:58 +0800

    Polish panel effect demo.

--- example/effect/PanelEffectDemo.dart
+++ example/effect/PanelEffectDemo.dart
@@ -18,18 +18,6 @@ void main() {
   container.profile.text = "width: content; height: content; location: center center";
   mainView.addChild(container);
   
-  final Button v1 = btn("Fade");
-  container.addChild(v1);
-  
-  final Button v2 = btn("Zoom");
-  container.addChild(v2);
-  
-  final Button v3 = btn("Slide");
-  container.addChild(v3);
-  
-  final Button v4 = btn("N/A");
-  container.addChild(v4);
-  
   Panel dialog;
   bool removeReady = false;
   EasingMotion removeMotion;
@@ -43,14 +31,15 @@ void main() {
     }
   };
   
-  dialog = new Panel()..addButton("close", (Event event) => _remove());
-  dialog.profile.text = "width: 50%; height: 50%; location: center center";
-  dialog.addChild(new TextView("Is this OK?")..profile.location = "center center"..style.fontSize = "14px");
-  Button okbtn = btn("OK")..profile.location = browser.mobile ? "bottom center" : "bottom right";
-  okbtn.on.click.add((ViewEvent event) => _remove());
-  dialog.addChild(okbtn);
+  dialog = new Panel()..addButton("close", (Event event) => _remove())
+      ..profile.text = "width: 50%; height: 50%; location: center center";
+  dialog.addChild(new TextView("Is this OK?")
+      ..profile.location = "center center"..style.fontSize = "14px");
+  dialog.addChild(btn("OK")
+      ..profile.location = browser.mobile ? "bottom center" : "bottom right"
+      ..on.click.add((ViewEvent event) => _remove()));
   
-  v1.on.click.add((ViewEvent event) {
+  container.addChild(btn("Fade")..on.click.add((ViewEvent event) {
     dialog.style.visibility = "hidden";
     dialog.addToDocument(mode: "dialog");
     final Element mask = dialog.maskNode;
@@ -65,9 +54,9 @@ void main() {
       dialog.remove();
       dialog.style.visibility = "";
     }, easing: (num t) => t * t, period: 300);
-  });
+  }));
   
-  v2.on.click.add((ViewEvent event) {
+  container.addChild(btn("Zoom")..on.click.add((ViewEvent event) {
     dialog.style.visibility = "hidden";
     dialog.addToDocument(mode: "dialog");
     final Element mask = dialog.maskNode;
@@ -82,12 +71,12 @@ void main() {
       dialog.remove();
       dialog.style.visibility = "";
     }, easing: (num t) => t * t, period: 300);
-  });
+  }));
   
-  v3.on.click.add((ViewEvent event) {
+  container.addChild(btn("Slide")..on.click.add((ViewEvent event) {
     dialog.style.visibility = "hidden";
     dialog.addToDocument(mode: "dialog");
-    dialog.requestLayout(true);
+    dialog.requestLayout(true); // as slide effect depends on initial size
     final Element mask = dialog.maskNode;
     
     new SlideInEffect(dialog.node,
@@ -100,12 +89,12 @@ void main() {
       dialog.remove();
       dialog.style.visibility = "";
     }, easing: (num t) => t * t, period: 300);
-  });
+  }));
   
-  v4.on.click.add((ViewEvent event) {
+  container.addChild(btn("N/A")..on.click.add((ViewEvent event) {
     dialog.addToDocument(mode: "dialog");
     removeReady = true;
     removeMotion = null;
-  });
+  }));
   
 }
