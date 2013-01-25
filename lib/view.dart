ec40ce412349681186c64d3f203e4a1c4219f2bc
simonpai <simonpai@potix.com>
2013-01-25 10:19:20 +0800

    Fine tune.

--- lib/view.dart
+++ lib/view.dart
@@ -2,41 +2,41 @@
 //Feb. 04, 2012
 library rikulo_view;
 
-import "dart:html";
-import "dart:math";
-import "dart:collection";
+import 'dart:html';
+import 'dart:math';
+import 'dart:collection';
 
 import 'package:rikulo_commons/util.dart';
 
-import "event.dart";
-import "html.dart";
+import 'event.dart';
+import 'html.dart';
 import 'gesture.dart';
-import "effect.dart";
-import "layout.dart";
-import "model.dart";
-import "view/impl.dart";
-import "view/select.dart";
+import 'effect.dart';
+import 'layout.dart';
+import 'model.dart';
+import 'view/impl.dart';
+import 'view/select.dart';
 
-part "src/view/View.dart";
-part "src/view/ViewUtil.dart";
-part "src/view/ProfileDeclaration.dart";
-part "src/view/LayoutDeclaration.dart";
-part "src/view/_impls.dart";
-part "src/view/_utils.dart";
-part "src/view/_StyleImpl.dart";
-part "src/view/printc.dart";
+part 'src/view/View.dart';
+part 'src/view/ViewUtil.dart';
+part 'src/view/ProfileDeclaration.dart';
+part 'src/view/LayoutDeclaration.dart';
+part 'src/view/_impls.dart';
+part 'src/view/_utils.dart';
+part 'src/view/_StyleImpl.dart';
+part 'src/view/printc.dart';
 
-part "src/view/Button.dart";
-part "src/view/Canvas.dart";
-part "src/view/CheckBox.dart";
-part "src/view/DropDownList.dart";
-part "src/view/Image.dart";
-part "src/view/Link.dart";
-part "src/view/Panel.dart";
-part "src/view/ScrollView.dart";
-part "src/view/Section.dart";
-part "src/view/Style.dart";
-part "src/view/Switch.dart";
-part "src/view/TextArea.dart";
-part "src/view/TextBox.dart";
-part "src/view/TextView.dart";
+part 'src/view/Button.dart';
+part 'src/view/Canvas.dart';
+part 'src/view/CheckBox.dart';
+part 'src/view/DropDownList.dart';
+part 'src/view/Image.dart';
+part 'src/view/Link.dart';
+part 'src/view/Panel.dart';
+part 'src/view/ScrollView.dart';
+part 'src/view/Section.dart';
+part 'src/view/Style.dart';
+part 'src/view/Switch.dart';
+part 'src/view/TextArea.dart';
+part 'src/view/TextBox.dart';
+part 'src/view/TextView.dart';
